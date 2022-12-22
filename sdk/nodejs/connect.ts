11c631f04343d5114a20708839be7a3210d1451c
jffarge <jf@dagger.io>
2022-12-22 11:56:50 +0100

    sdk: nodejs: immprove connect doc reference
    Signed-off-by: jffarge <jf@dagger.io>

--- sdk/nodejs/connect.ts
+++ sdk/nodejs/connect.ts
@@ -6,8 +6,25 @@ import { Writable } from "node:stream"
  * ConnectOpts defines option used to connect to an engine.
  */
 export interface ConnectOpts {
+  /**
+   * Use to override Dagger workdir
+   */
   Workdir?: string
+  /**
+   * Use to override Dagger config
+   */
   ConfigPath?: string
+  /**
+   * Enable logs output
+   * @example
+   * LogOutput
+   * ```ts
+   * connect(async (client: Client) => {
+    const source = await client.host().workdir().id()
+    ...
+    }, {LogOutput: process.stdout})
+    ```
+   */
   LogOutput?: Writable
 }
 
